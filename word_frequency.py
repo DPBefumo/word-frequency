912cc408cd689c962634c1352a0fb71f96c86415
Danny Befumo <DPBefumo@gmail.com>
2020-05-21 10:49:34 -0400

    * alignment fixed, code cleaned up a little, still organized alphabetically, not numerically

--- word_frequency.py
+++ word_frequency.py
@@ -1,40 +1,36 @@
+import string
+
 STOP_WORDS = [
     'a', 'an', 'and', 'are', 'as', 'at', 'be', 'by', 'for', 'from', 'has', 'he',
     'i', 'in', 'is', 'it', 'its', 'of', 'on', 'that', 'the', 'to', 'were',
     'will', 'with'
 ]
 
-import string
-punctuation = string.punctuation
-
 def print_word_freq(file):
     opened_file = open(file)
     text = opened_file.read()
-    
+
     no_punctuation = ""
     for character in text:
-        if character not in punctuation:
+        if character not in string.punctuation:
             no_punctuation = no_punctuation + character
     
     text_lower = no_punctuation.lower()
-    
+
     split_text = text_lower.split()
     
     new_text = [n_text for n_text in split_text if not n_text in STOP_WORDS]
     
-    repeated_text = dict()
+    word_freq = {}
     for word in new_text:
-        if word in repeated_text:
-            repeated_text[word] += 1
-        else:
-            repeated_text[word] = 1
+        word_freq[word] = word_freq.get(word, 0) +1
+
+    word_freq = {key: value for key, value in word_freq.items() if value >=6}
     
-    repeated_text = {key: value for key, value in repeated_text.items() if value >=6}
-    
-    sorted_text = sorted(repeated_text.items(), key=lambda seq: seq[0], reverse=False)
+    sorted_text = sorted(word_freq.items(), key=lambda seq: seq[0], reverse=False)
     
     for key, value in sorted_text:
-        print (key.rjust(15) , ' | ', value, value * ('*'))
+        print (key.rjust(15) , ' | ', str(value).ljust(2), value * ('*'))
 
 if __name__ == "__main__":
     import argparse
